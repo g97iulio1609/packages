cb5784b485a0f7422db17e9c7d06b2f060ea88f6
Giulio Leone <giulio97.leone@gmail.com>
2015-05-24 11:40:37 +0200

    first commit

---
+++ calamares/src/calamares-cc8f6c01d7bd0dd9dfa11a7aa4052b4513997b16/src/calamares/progresstree/ProgressTreeDelegate.cpp
@@ -0,0 +1,147 @@
+/* === This file is part of Calamares - <http://github.com/calamares> ===
+ *
+ *   Copyright 2014, Teo Mrnjavac <teo@kde.org>
+ *
+ *   Calamares is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Calamares is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ProgressTreeDelegate.h"
+
+#include "../CalamaresApplication.h"
+#include "../CalamaresWindow.h"
+#include "ViewStepItem.h"
+#include "ProgressTreeModel.h"
+#include "ViewManager.h"
+#include "Branding.h"
+#include "utils/CalamaresUtilsGui.h"
+
+#include <QAbstractItemView>
+#include <QPainter>
+
+#define ITEM_MARGIN 12
+#define VS_FONTSIZE CalamaresUtils::defaultFontSize() + 2
+#define CAT_FONTSIZE CalamaresUtils::defaultFontSize() + 5
+
+ProgressTreeDelegate::ProgressTreeDelegate( QAbstractItemView* parent )
+    : QStyledItemDelegate( parent )
+    , m_parent( parent )
+{
+}
+
+
+QSize
+ProgressTreeDelegate::sizeHint( const QStyleOptionViewItem& option,
+                                const QModelIndex& index ) const
+{
+    if ( !index.isValid() )
+        return option.rect.size();
+
+    bool isFirstLevel = !index.parent().isValid();
+
+    QFont font = qApp->font();
+
+    if ( isFirstLevel )
+    {
+        font.setPointSize( CAT_FONTSIZE );
+    }
+    else
+    {
+        font.setPointSize( VS_FONTSIZE );
+    }
+    QFontMetrics fm( font );
+    int height = fm.height();
+
+    height += 2*ITEM_MARGIN; //margin
+
+    return QSize( option.rect.width(), height );
+}
+
+
+void
+ProgressTreeDelegate::paint( QPainter* painter,
+                             const QStyleOptionViewItem& option,
+                             const QModelIndex& index) const
+{
+    bool isFirstLevel = !index.parent().isValid();
+
+    QStyleOptionViewItemV4 opt = option;
+
+    painter->save();
+
+    initStyleOption( &opt, index );
+    opt.text.clear();
+
+    painter->setBrush( QColor( Calamares::Branding::instance()->
+        styleString( Calamares::Branding::SidebarBackground ) ) );
+    painter->setPen( QColor( Calamares::Branding::instance()->
+        styleString( Calamares::Branding::SidebarText ) ) );
+
+    if ( isFirstLevel )
+        paintCategory( painter, opt, index );
+    else
+        paintViewStep( painter, opt, index );
+
+    painter->restore();
+}
+
+
+void
+ProgressTreeDelegate::paintCategory( QPainter* painter,
+                                     const QStyleOptionViewItem& option,
+                                     const QModelIndex& index ) const
+{
+    QRect textRect = option.rect.adjusted( ITEM_MARGIN,
+                                           ITEM_MARGIN,
+                                           ITEM_MARGIN,
+                                           ITEM_MARGIN );
+
+    bool isCurrent = index.data( ProgressTreeModel::ProgressTreeItemCurrentRole ).toBool();
+
+    QFont font = qApp->font();
+    font.setPointSize( CAT_FONTSIZE );
+    font.setBold( false );
+    font.setUnderline( isCurrent ); // FIXME: Figure out a nicer way to highlight the current category step
+    painter->setFont( font );
+
+    painter->drawText( textRect, index.data().toString() );
+}
+
+
+void
+ProgressTreeDelegate::paintViewStep( QPainter* painter,
+                                     const QStyleOptionViewItem& option,
+                                     const QModelIndex& index ) const
+{
+    QRect textRect = option.rect.adjusted( ITEM_MARGIN + 32 /*indentation*/,
+                                           ITEM_MARGIN,
+                                           ITEM_MARGIN,
+                                           ITEM_MARGIN );
+    QFont font = qApp->font();
+    font.setPointSize( VS_FONTSIZE );
+    font.setBold( false );
+    painter->setFont( font );
+
+    bool isCurrent = false;
+    isCurrent = index.data( ProgressTreeModel::ProgressTreeItemCurrentRole ).toBool();
+
+    if ( isCurrent )
+    {
+        painter->setPen( Calamares::Branding::instance()->
+                         styleString( Calamares::Branding::SidebarTextSelect ) );
+        painter->setBrush( APP->mainWindow()->palette().background() );
+    }
+
+    painter->fillRect( option.rect, painter->brush().color() );
+    painter->drawText( textRect, index.data().toString() );
+}
---
+++ calamares/src/calamares-cc8f6c01d7bd0dd9dfa11a7aa4052b4513997b16/src/modules/partition/jobs/ClearMountsJob.cpp
@@ -0,0 +1,88 @@
+/* === This file is part of Calamares - <http://github.com/calamares> ===
+ *
+ *   Copyright 2014-2015, Teo Mrnjavac <teo@kde.org>
+ *
+ *   Calamares is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *
+ *   Calamares is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with Calamares. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "ClearMountsJob.h"
+
+#include <core/device.h>
+#include <core/partition.h>
+#include <core/PartitionInfo.h>
+#include <core/PartitionIterator.h>
+#include <util/report.h>
+#include <utils/Logger.h>
+
+#include <QProcess>
+#include <QStringList>
+
+
+ClearMountsJob::ClearMountsJob( Device* device )
+    : Calamares::Job()
+    , m_device( device )
+{
+}
+
+
+QString
+ClearMountsJob::prettyName() const
+{
+    return tr( "Clear mounts for partitioning operations on %1" )
+            .arg( m_device->deviceNode() );
+}
+
+
+Calamares::JobResult
+ClearMountsJob::exec()
+{
+    QStringList goodNews;
+
+    QProcess process;
+    process.setProgram( "sh" );
+    process.setArguments( {
+                              "-c",
+                              QString( "echo $(awk '{print $4}' /proc/partitions | sed -e '/name/d' -e '/^$/d' -e '/[1-9]/!d' | grep %1)" )
+                                      .arg( m_device->deviceNode().split( '/' ).last() )
+                          } );
+    process.start();
+    process.waitForFinished();
+
+    QString partitions = process.readAllStandardOutput();
+    QStringList partitionsList = partitions.simplified().split( ' ' );
+
+    foreach ( QString p, partitionsList )
+    {
+        QString partPath = QString( "/dev/%1" ).arg( p );
+
+        process.start( "umount", { partPath } );
+        process.waitForFinished();
+        if ( process.exitCode() == 0 )
+            goodNews.append( QString( "Successfully unmounted %1." ).arg( partPath ) );
+
+        process.start( "swapoff", { partPath } );
+        process.waitForFinished();
+        if ( process.exitCode() == 0 )
+            goodNews.append( QString( "Successfully disabled swap %1." ).arg( partPath ) );
+    }
+
+    Calamares::JobResult ok = Calamares::JobResult::ok();
+    ok.setMessage( tr( "Cleared all mounts for %1" )
+                        .arg( m_device->deviceNode() ) );
+    ok.setDetails( goodNews.join( "\n" ) );
+
+    cDebug() << "ClearMountsJob finished. Here's what was done:\n" << goodNews.join( "\n" );
+
+    return ok;
+}
